4f4d1136d8dcc932f972865c32ee95d5b093d4f6
Владислав Курочкин <vlad555.95@mail.ru>
2018-04-08 01:26:20 +0400

    done desktop

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,20 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
+const wait = require('gulp-wait');
 
 const config = {
     js: {
-        src: './statc/scripts/js/**/*.js',
-        dest: './static/scripts/'
+        src: './src/scripts/js/**/*.js',
+        dest: './dist/js/'
     },
     scss: {
-        src: './static/styles/scss/main.scss',
-        dest: './static/styles/'
+        src: './src/scss/main.scss',
+        dest: './dist/styles/'
+    },
+    img: {
+        src: './src/img/**',
+        dest: './dist/img/'
     }
 }
 
@@ -18,28 +23,38 @@ const config = {
 gulp.task('browser-sync', () =>
     browserSync.init({
         server: {
-            baseDir: "./static"
+            baseDir: "./"
         }
     })
 );
 
 gulp.task('js', () =>
     gulp.src(config.js.src)
+    .pipe(wait(500))
     .pipe(concat('main.js'))
     .pipe(gulp.dest(config.js.dest))
 );
 
 gulp.task('sass', () =>
     gulp.src(config.scss.src)
+    .pipe(wait(500))
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest(config.scss.dest))
     .pipe(browserSync.stream())
 );
 
+gulp.task('img', () => 
+    gulp.src(config.img.src)
+    .pipe(gulp.dest(config.img.dest))
+);
+
 gulp.task('watch', () => {
-    gulp.watch('./static/styles/scss/**/*.scss', ['sass']);
-    gulp.watch('./static/scripts/**/*.js', ['js']);
-    gulp.watch('./static/index.html', browserSync.reload);
+    gulp.watch('./src/scss/**/*.scss', ['sass']);
+    gulp.watch('./src/js/**/*.js', ['js']);
+    gulp.watch(config.img.src, ['img']);
+    gulp.watch('./index.html', browserSync.reload);
 });
 
-gulp.task('default', ['sass', 'js', 'watch', 'browser-sync']);
+gulp.task('prepare', ['sass', 'js', 'img']);
+
+gulp.task('default', ['prepare', 'watch', 'browser-sync']);
